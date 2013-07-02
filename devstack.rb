7991d5269801480188cf3827a573fe18a8692fef
Graeme <graeme.wilson@iplayup.com>
2013-07-02 16:16:36 +1000

    initial run at babushka-izing devstack

---
+++ devstack.rb
@@ -0,0 +1,70 @@
+
+dep 'devstack.repo' do
+  met? { File.exists?(path) }
+  meet { git source, :to => path }
+end
+
+dep 'devstack.variable_file' do
+  met? { variable_path. p.exists? }
+  meet { shell "touch #{variable_path}" }
+end
+
+dep 'devstack.admin_password' do 
+  requires 'devstack.variable_file'
+  met? {has_variable?("ADMIN_PASSWORD") }
+  meet { set_variable("ADMIN_PASSWORD","password") }
+end
+
+dep 'devstack.mysql_password' do 
+  requires 'devstack.variable_file'
+  met? {has_variable?("MYSQL_PASSWORD") }
+  meet { set_variable("MYSQL_PASSWORD","password") }
+end
+
+dep 'devstack.rabbit_password' do 
+  requires 'devstack.variable_file'
+  met? {has_variable?("RABBIT_PASSWORD") }
+  meet { set_variable("RABBIT_PASSWORD","password") }
+end
+
+dep 'devstack.service_password' do 
+  requires 'devstack.variable_file'
+  met? {has_variable?("SERVICE_PASSWORD") }
+  meet { set_variable("SERVICE_PASSWORD","password") }
+end
+
+dep 'devstack.service_token' do 
+  requires 'devstack.variable_file'
+  met? {has_variable?("SERVICE_TOKEN") }
+  meet { set_variable("SERVICE_TOKEN","tokentoken") }
+end
+
+
+
+dep 'devstack' do 
+  dep 'devstack.repo'
+  dep 'devstack.admin_password'
+  dep 'devstack.mysql_password'
+  dep 'devstack.rabbit_password'
+  dep 'devstack.service_password'
+  dep 'devstack.service_token'
+end
+
+def set_variable(name, value)
+  variable_path.p.append("#{name}=#{value}\n")
+end
+
+def has_variable?(name)
+  variable_path.p.read.include?(name)
+end
+
+def path
+  File.expand_path '~/devstack'
+end
+def source
+  'https://github.com/openstack-dev/devstack.git'
+end
+
+def variable_path
+  path+'/localrc'
+end
